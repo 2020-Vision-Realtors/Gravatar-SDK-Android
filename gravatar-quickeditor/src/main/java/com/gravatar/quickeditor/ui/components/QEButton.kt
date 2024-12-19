4afc8270a8214d45c2cdabb045979da1125bd652
Héctor Abraham <hector.abraham.morillo.prieto@automattic.com>
2024-12-19 16:43:57 +0100

    Add loading option to the QEButton
    In the Figma file the height is fixed to 50.dp

--- gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/ui/components/QEButton.kt
+++ gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/ui/components/QEButton.kt
@@ -3,8 +3,10 @@ package com.gravatar.quickeditor.ui.components
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -22,19 +24,25 @@ internal fun QEButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
+    loading: Boolean = false,
 ) {
     Button(
         modifier = modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .size(50.dp),
         onClick = onClick,
         shape = RoundedCornerShape(4.dp),
-        contentPadding = PaddingValues(14.dp),
-        enabled = enabled,
+        contentPadding = if (loading) PaddingValues(8.dp) else PaddingValues(horizontal = 14.dp),
+        enabled = enabled && !loading,
     ) {
-        Text(
-            text = buttonText,
-            style = MaterialTheme.typography.titleMedium,
-        )
+        if (loading) {
+            CircularProgressIndicator(Modifier.size(32.dp))
+        } else {
+            Text(
+                text = buttonText,
+                style = MaterialTheme.typography.titleMedium,
+            )
+        }
     }
 }
 
@@ -48,6 +56,22 @@ private fun QEButtonPreview() {
                 buttonText = stringResource(id = R.string.gravatar_qe_avatar_picker_upload_image),
                 onClick = { },
                 enabled = true,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun QEButtonLoadingPreview() {
+    GravatarTheme {
+        Surface {
+            QEButton(
+                modifier = Modifier.padding(20.dp),
+                buttonText = stringResource(id = R.string.gravatar_qe_avatar_picker_upload_image),
+                onClick = { },
+                enabled = false,
+                loading = true,
             )
         }
     }
